6b9cd8ba4c0f06f7b7e1753886f1a8c2a3a6b063
samsullivan878 <samsullivan878@gmail.com>
2019-09-18 19:27:23 -0600

    mandaotry par2t

--- lab3_script.sh
+++ lab3_script.sh
@@ -6,4 +6,4 @@ echo "enter regular expression: "
 read exp
 grep $exp $fileName
 echo "Number of phone numbers: "
-grep -c '[0-9]\{3\}\-[0-9]\{3\}\-[0-9]\{4\}' regex_practice.txt'
+grep -c '[0-9]\{3\}\-[0-9]\{3\}\-[0-9]\{4\}' regex_practice.txt
